92869875e6d5bc7140fb9f53efdad08bb6e72c53
Sreeraj <raj.nair72000@gmail.com>
2024-07-30 15:22:37 +0530

    Made changes in drizzle config

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://ai-app_owner:bD4VC8ZWBopM@ep-square-limit-a5k45g6c.us-east-2.aws.neon.tech/ai-app?sslmode=require',
-    }
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
+}
   };
