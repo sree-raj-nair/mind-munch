48b1eb780fe992d402cfa697aad52394f3b2cfa9
Sreeraj <raj.nair72000@gmail.com>
2024-08-12 23:01:38 +0530

    Re-deploying

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
           <Button className="hidden lg:inline-flex">Try Now</Button>
         </Link>
       </header>
-        {/* Hero Section */}
+      
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-gradient-to-r from-purple-500 to-indigo-600">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
